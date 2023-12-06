45c1f28a19f6e85edb8e131b4b938d75904c4289
Fundevoge <59122389+Fundevoge@users.noreply.github.com>
2023-12-06 10:13:15 +0100

    Day 6

---
+++ src/bin/d6.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // Solve t^2 - T*t + D = 0, lower solution rounded up and higher solution rounded down
+    // give the inclusive range
+    println!("Solution 1: 800280");
+    println!("Solution 2: 45128024");
+}
